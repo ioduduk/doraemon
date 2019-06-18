ef45d94bd7e15e923fcaa7fc544f18a710ea635a
何承雄 <hechengxiong@yunniao.me>
2019-06-18 12:53:02 +0800

    min_transform

---
+++ min_transform/min_transform.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def F(a, b):
+    if not a:
+        return len(b)
+
+    if not b:
+        return len(a)
+
+    if a == b:
+        return 0
+
+    if a[-1] == b[-1]:
+        return F(a[0:-1], b[0:-1])
+
+    return min(
+            F(a[0:-1], b[0:-1]) + 1,
+            F(a[0:-1], b) + 1
+            )
+
+
+if __name__ == '__main__':    
+    print(F('horse', 'ers'))
+    print(F('panckey', 'epan'))
